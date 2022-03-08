df7b0c642509b947b6fc433d331f6a66dd17545c
Jesse Lind <Jesseclind@gmail.com>
2022-03-07 20:04:00 -0500

    fix: thrids now included in servings fractions.
    closes #32

--- src/util/calcServings.js
+++ src/util/calcServings.js
@@ -5,9 +5,28 @@ const mixedToDecimal = str => {
 
   return split.reduce((prev, curr) => evalNum(prev) + evalNum(curr), 0)
 }
+const decimalToFraction = num => {
+  const fracs = [
+    { frac: '0', num: 0 },
+    { frac: '1/8', num: 0.125 },
+    { frac: '1/4', num: 0.25 },
+    { frac: '1/3', num: 0.333 },
+    { frac: '3/8', num: 0.375 },
+    { frac: '1/2', num: 0.5 },
+    { frac: '5/8', num: 0.625 },
+    { frac: '2/3', num: 0.666 },
+    { frac: '3/4', num: 0.75 },
+    { frac: '7/8', num: 0.875 },
+  ]
+
+  const closest = fracs.sort(
+    (a, b) => Math.abs(num - a.num) - Math.abs(num - b.num)
+  )[0]
+  return closest.frac
+}
 
 export const calcServings = (ingredients, originalServings, newServings) => {
-  const fraction = newServings / originalServings
+  const fractionMulti = newServings / originalServings
 
   const gcd = function (a, b) {
     if (!b) return a
@@ -20,23 +39,18 @@ export const calcServings = (ingredients, originalServings, newServings) => {
       if (ingr.quantity) {
         const denominator = 16
 
-        const res = mixedToDecimal(ingr.quantity) * fraction
+        const res = mixedToDecimal(ingr.quantity) * fractionMulti
         const wholeNum = Math.floor(res)
-        let numerator = Math.floor((res - wholeNum) * denominator)
+        let fraction = decimalToFraction(res - wholeNum)
 
-        if (numerator === 0) {
+        if (fraction === '0') {
           return { ...ingr, quantity: `${wholeNum}` }
         }
-        const fractionGCD = gcd(numerator, denominator)
-        const quantityFraction =
-          fractionGCD !== 1
-            ? `${numerator / fractionGCD}/${denominator / fractionGCD}`
-            : `${numerator}/${denominator}`
 
         if (wholeNum === 0) {
-          return { ...ingr, quantity: `${quantityFraction}` }
+          return { ...ingr, quantity: `${fraction}` }
         }
-        return { ...ingr, quantity: `${wholeNum} ${quantityFraction}` }
+        return { ...ingr, quantity: `${wholeNum} ${fraction}` }
       }
       return { ...ingr }
     })
