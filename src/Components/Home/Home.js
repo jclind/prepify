9f6778118f2e193cdfb3b24b289f8fe09d948356
Jesse Lind <Jesseclind@gmail.com>
2022-03-09 15:03:10 -0500

    fix: clean Home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,14 +4,6 @@ import TrendingRecipes from '../TrendingRecipes/TrendingRecipes'
 import { Helmet } from 'react-helmet'
 
 const Home = () => {
-  const handleFetch = async () => {
-    // let test = await fetch(
-    //   `https://api.edamam.com/api/nutrition-data?app_id=${process.env.REACT_APP_EDAMAM_APP_ID}&app_key=${process.env.REACT_APP_EDAMAM_APP_KEY}&nutrition-type=cooking&ingr=1%201%2F2%20pound%20Bonelss%20skinless%20chicken%20breasts%20%2C%20diced%20into%203%2F4-inch%20pieces`,
-    //   { mode: 'no-cors' }
-    // )
-    // console.log(test)
-  }
-
   return (
     <>
       <Helmet>
@@ -21,7 +13,6 @@ const Home = () => {
       <div className='page home-page'>
         <HomeHero />
         <TrendingRecipes />
-        <button onClick={handleFetch}>Get Data</button>
       </div>
     </>
   )
