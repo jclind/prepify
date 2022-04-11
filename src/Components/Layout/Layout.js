bdd955212e75987865c7ef60cc03c0c0bd1c152b
Jesse Lind <Jesseclind@gmail.com>
2022-04-11 10:44:51 -0400

    fix: page now scolled to top on route change closes #10

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    document.querySelector('.nav').scrollIntoView()
+  }, [pathname])
+  return null
+}
 
 const Layout = ({ children, darkNavLinks }) => {
   return (
     <>
       <Navbar darkNavLinks={darkNavLinks} />
+      <ScrollToTop />
       {children}
       <Footer />
     </>
