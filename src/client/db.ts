02b92824d9625784de9d843d14cd20c9c7363f0c
Jesse Lind <jesseclind@gmail.com>
2026-05-09 23:59:15 -0400

    WIP

--- src/client/db.ts
+++ src/client/db.ts
@@ -1,8 +1,5 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, signInWithCustomToken } from 'firebase/auth'
-import { getStorage } from 'firebase/storage'
-import { getFirestore } from 'firebase/firestore'
-import { getAnalytics } from 'firebase/analytics'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -14,11 +11,8 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
-export const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
-export const storage = getStorage(app)
-export const db = getFirestore(app)
-export const analytics = getAnalytics(app)
 
 if (import.meta.env.VITE_CYPRESS === 'true') {
   ;(window as any).__cy_signIn__ = (token: string) => signInWithCustomToken(auth, token)
